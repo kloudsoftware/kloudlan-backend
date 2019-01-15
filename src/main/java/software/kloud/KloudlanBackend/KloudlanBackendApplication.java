6a5b04603b84e02426128e78e2e2751b3b9ebf57
lars <lars@kloud.software>
2019-01-15 19:00:55 +0100

    init

---
+++ src/main/java/software/kloud/KloudlanBackend/KloudlanBackendApplication.java
@@ -0,0 +1,14 @@
+package software.kloud.KloudlanBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KloudlanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KloudlanBackendApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/software/kloud/KloudlanBackend/KloudlanBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package software.kloud.KloudlanBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KloudlanBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
